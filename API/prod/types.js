e29c0ae2b03d4558e560faa55a284b58505c69b6
htds <harry.t.dev@gmail.com>
2020-05-05 11:39:54 +0100

    Updated build.js

--- API/prod/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TYPES = {
-    TrackRepository: Symbol('TrackRepository'),
-    TrackService: Symbol('TrackService'),
-    Controller: Symbol('Controller')
-};
-exports.default = TYPES;
--- Util/build.js
+++ Util/build.js
@@ -1,22 +1,24 @@
 const fs = require('fs-extra');
-const childProcess = require('child_process');
+const {exec} = require('child_process');
 
 try {
-    // build vue SPA 
-    childProcess.exec('npm run build',  {
-        cwd: './Web/'
+    // // build vue SPA 
+    exec('npm run build',  {
+        cwd: '../Web/'
+    } );
+
+    //build nodejs server ts to js
+    exec('npm run build',  {
+        cwd: '../API/'
+    }, function() {
+        // Remove prev build and copy files to /prod
+        fs.removeSync('../prod/');
+        fs.copySync('../API/prod', '../prod/');
+        fs.copySync('../API/node_modules', '../prod/node_modules');
+        fs.copySync('../API/public', '../prod/public');
+        fs.copySync('../API/data', '../prod/data');
+        fs.removeSync('../prod/tests');
     });
-    // build nodejs server ts to js
-    childProcess.exec('npm run build',  {
-        cwd: './API/'
-    });
-    // Remove prev build and copy files to /prod
-    fs.removeSync('../prod/');
-    fs.copySync('../API/prod', '../prod/');
-    fs.copySync('../API/node_modules', '../prod/node_modules');
-    fs.copySync('../API/public', '../prod/public');
-    fs.copySync('../API/data', '../prod/data');
-    fs.removeSync('../prod/tests');
 } catch (err) {
     console.log(err);
 }
