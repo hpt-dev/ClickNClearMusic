d73f658061e29bb9b833154a32bf8f0bb3746b9e
htds <harry.t.dev@gmail.com>
2020-05-05 07:41:57 +0100

    added modules to build.js

--- Util/build.js
+++ Util/build.js
@@ -13,6 +13,7 @@ try {
     // Remove current build and copy files to /prod
     fs.removeSync('../prod/');
     fs.copySync('../API/prod', '../prod/');
+    fs.copySync('../API/node_modules', '../prod/node_modules');
     fs.copySync('../API/public', '../prod/public');
     fs.copySync('../API/data', '../prod/data');
     fs.removeSync('../prod/tests');
